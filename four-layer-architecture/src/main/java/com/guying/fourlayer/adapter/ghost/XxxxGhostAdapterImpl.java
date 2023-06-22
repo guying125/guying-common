d7b48d82f7af4852a19dc8fd7e5e9ff2322b9eea
guying125 <2430713057@qq.com>
2023-06-22 09:31:21 +0800

    四层架构

---
+++ four-layer-architecture/src/main/java/com/guying/fourlayer/adapter/ghost/XxxxGhostAdapterImpl.java
@@ -0,0 +1,17 @@
+package com.guying.fourlayer.adapter.ghost;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * Ghost适配器实现类
+ *
+ * @author Code Farmer
+ * @since current time
+ */
+@Slf4j
+@Service
+public class XxxxGhostAdapterImpl {
+//public class XxxxGhostAdapterImpl implements GhostAdapter {
+
+}
